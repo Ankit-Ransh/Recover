cdcc6d97d7e8cc880c257517b0850d892d24f912
Ankit-Ransh <ankitanand2125@gmail.com>
2024-04-16 17:43:43 +0530

    Remove lib/core/secrets from tracked files

--- lost_found/lib/core/secrets/supabase_secrets.dart
+++
@@ -1,5 +0,0 @@
-class SupaBaseSecrets {
-  static const url = "https://klowthrftrzieqfvnfym.supabase.co";
-  static const anonKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtsb3d0aHJmdHJ6aWVxZnZuZnltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMyNTc5MTgsImV4cCI6MjAyODgzMzkxOH0.j2Ibzyzd2XUzXfxgaJ9yCeHJtFs9EJjkSYtRkk8qZvs";
-}
