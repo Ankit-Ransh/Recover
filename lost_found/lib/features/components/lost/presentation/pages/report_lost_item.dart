4520729c75a90f69bb6fad821e71d74e5c72ae05
Ankit-Ransh <ankitanand2125@gmail.com>
2024-04-20 18:28:51 +0530

    Icons configuration added

--- lost_found/lib/features/components/lost/presentation/pages/report_lost_item.dart
+++ lost_found/lib/features/components/lost/presentation/pages/report_lost_item.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lost_found/core/common/cubit/app_user/app_user_cubit.dart';
 import 'package:lost_found/core/common/widgets/loader.dart';
